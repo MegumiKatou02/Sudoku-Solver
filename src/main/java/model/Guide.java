26ea5ed23eed6b504d098b9da32375e18a4bac3d
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-11-17 11:08:05 +0700

    release

--- src/main/java/model/Guide.java
+++ src/main/java/model/Guide.java
@@ -6,7 +6,8 @@ public class Guide {
     public Guide() {
         str = new StringBuilder("<html><pre>");
         str.append(" - Input theo dạng 9 x 9 <br>");
-        str.append(" - Khoảng cách giữa các số là khoảng <br>   trắng <br>");
+        str.append(" - Khoảng cách giữa các số là khoảng <br>   trắng");
+        str.append(" hoặc có thể viết dính liền <br>   nhau<br>");
         str.append(" - Các số bị thiếu điền dấu .");
         str.append("<pre></html>");
     }
